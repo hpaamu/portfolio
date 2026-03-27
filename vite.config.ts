d3fc4927900f4bba52568b83b8aa11a87b5a9da0
hpaamu <pharika0605@gmail.com>
2026-03-26 23:58:39 -0500

    ghpages fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-client-harika-paamu/',
+  base: '/portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
